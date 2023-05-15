2dfc0ac8f1b07916de331ee2dba09cbf5b5339b7
Marcelo Carvalho Ferraz <carvalhoferrazmarcelo@gmail.com>
2023-05-15 17:59:50 -0300

    updated timings to respect bad internet connections loading times

--- utils/automation.py
+++ utils/automation.py
@@ -63,23 +63,23 @@ def start(channels_dict):
     for channel in channels_dict.keys():
         emails = channels_dict[channel]
 
-        # click search input 2x to make sure it the search input was selected
-        for i in range (0, 2):
+        # click search input 3x to make sure it the search input was selected
+        for i in range (0, 3):
             gui.clickSleep(
                 COORDINATES["search_input"][0], # x
                 COORDINATES["search_input"][1], # y
-                common.STANDARD_SLEEP_TIME / 3 # 0,33s
+                common.STANDARD_SLEEP_TIME / 2 # 0,3s
             )
         # select and delete all text in the search field
         gui.hotKeySleep(
             "ctrl", "a",
-            common.STANDARD_SLEEP_TIME / 5 # 0,2s
+            common.STANDARD_SLEEP_TIME / 3 # 0,33s
         )
         gui.pressKey("del")
         # type channel name on search input
         gui.typeSleep(
             channel, # channel name
-            common.STANDARD_SLEEP_TIME / 5 # 0,2s
+            common.STANDARD_SLEEP_TIME / 2 # 0,5s
         )
         # press enter to search for it
         gui.pressKeySleep(
@@ -102,20 +102,20 @@ def start(channels_dict):
         gui.clickSleep(
             COORDINATES["members"][0], # x
             COORDINATES["members"][1], # y
-            common.STANDARD_SLEEP_TIME / 5 # 0,2s
+            common.STANDARD_SLEEP_TIME / 5 # 0,5s
         )
         # click add people
         gui.clickSleep(
             COORDINATES["add_people"][0], # x
             COORDINATES["add_people"][1], # y
-            common.STANDARD_SLEEP_TIME / 5 # 0,2s
+            common.STANDARD_SLEEP_TIME / 3 # 0,33s
         )
         # click the e-mails input 2x to make sure it was clicked
         for i in range (0, 2):
             gui.clickSleep(
                 COORDINATES["emails_input"][0], # x
                 COORDINATES["emails_input"][1], # y
-                common.STANDARD_SLEEP_TIME / 5 # 0,2s
+                common.STANDARD_SLEEP_TIME / 3 # 0,33s
             )
         for email in emails:
             # type e-mail to add to the channel
@@ -126,12 +126,12 @@ def start(channels_dict):
             # press enter
             gui.pressKeySleep(
                 "enter", 
-                common.STANDARD_SLEEP_TIME / 2 # 0,5s
+                common.STANDARD_SLEEP_TIME / 3 # 0,33s
             )
         # press enter to go to next section
         gui.pressKeySleep(
             "enter", 
-            common.STANDARD_SLEEP_TIME / 2 # 0,5s
+            common.STANDARD_SLEEP_TIME / 3 # 0,33s
         )
         # steps bellow can repeat up to 2 times 👇🏻 (if the e-mail provider is not one in your workspaces e.g "@milezero.com", "@capstonelogistics.com" and "@lean-tech.io")
         repeats = 2 if checkIfNeedsRepetition(emails) else 1
@@ -140,13 +140,13 @@ def start(channels_dict):
             gui.clickSleep(
                 COORDINATES["from_another_organization"][0], # x
                 COORDINATES["from_another_organization"][1], # y
-                common.STANDARD_SLEEP_TIME / 5 # 0,2s
+                common.STANDARD_SLEEP_TIME / 3 # 0,33s
             )
             # click "Next"
             gui.clickSleep(
                 COORDINATES["from_another_organization_next"][0], # x
                 COORDINATES["from_another_organization_next"][1], # y
-                common.STANDARD_SLEEP_TIME / 5 # 0,2s
+                common.STANDARD_SLEEP_TIME / 3 # 0,33s
             )
             # click "Got it" (same position as Next)
             gui.clickSleep(
@@ -158,7 +158,7 @@ def start(channels_dict):
             gui.clickSleep(
                 COORDINATES["only_post"][0], # x
                 COORDINATES["only_post"][1], # y
-                common.STANDARD_SLEEP_TIME / 5 # 0,2s
+                common.STANDARD_SLEEP_TIME / 3 # 0,33s
             )
             # click "Next"
             gui.clickSleep(
@@ -173,18 +173,18 @@ def start(channels_dict):
             )
             gui.hotKeySleep(
                 "ctrl", "a",
-                common.STANDARD_SLEEP_TIME / 5 # 0,2s
+                common.STANDARD_SLEEP_TIME / 3 # 0,33s
             )
             gui.pressKey("del")
             gui.typeSleep(
                 "Requested by dispatcher", # note message
-                common.STANDARD_SLEEP_TIME * 0 # 0s
+                common.STANDARD_SLEEP_TIME / 5 # 0,2s
             )
             # click "Send invitations" or "Next" (accordingly to the number of repeats)
             gui.clickSleep(
                 COORDINATES["notes_send_invitation"][0], # x
                 COORDINATES["notes_send_invitation"][1], # y
-                common.STANDARD_SLEEP_TIME # 1s
+                common.STANDARD_SLEEP_TIME * 1.5 # 1,5s
             )
         # Press enter
         gui.pressKeySleep(
